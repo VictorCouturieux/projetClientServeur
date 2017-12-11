1aeb920a30bdfb33d55a3ac35b6cd40344660f0c
JcBouhel <jeanchristophe.bouhelier3@gmail.com>
2017-12-11 12:13:32 +0100

    Modif P2Pfunstions

--- src/comServCli/P2PFunctions.java
+++ src/comServCli/P2PFunctions.java
@@ -6,8 +6,10 @@ public class P2PFunctions {
 	
 	public static String printSearch(ArrayList<P2PFile> result) {
 		String print = "<";
+		int count = 1;
 		for (P2PFile p2pFile : result) {
-			print += p2pFile.toString() + "\n";
+			print += count + p2pFile.toString() + "\n";
+			count++;
 		}
 		print += ">";
 		
