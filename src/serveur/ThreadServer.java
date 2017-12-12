932fe35465e21858a1d48fd250a52e1119aa9e8d
VictorCouturieux <victor.couturieux@edu.univ-fcomte.fr>
2017-12-12 11:20:15 +0100

    DO WHILE

--- src/serveur/ThreadServer.java
+++ src/serveur/ThreadServer.java
@@ -58,51 +58,55 @@ public class ThreadServer extends Thread {
             lfs.addListFiles(lfc, sockComm.getRemoteSocketAddress());
             
             System.out.println(lfs.toString());
-            
-            Request requete = (Request)sockIn.readObject();
-           
-            ArrayList<P2PFile> currentSearch = new ArrayList<P2PFile>();
-            String commande = requete.getCommande();
-            
-            switch (commande) {
-			case "list":
-				sockOs.writeObject(currentSearch);
-				sockOs.flush();
-				break;
-				
-			case "help":
-				sockOs.writeUTF(" search <pattern> \n get <num> \n list \n local list \n quit");
-				sockOs.flush();
-				break;
-				
-			case "search":
-				String motif = requete.getArg();
-				System.out.println(motif);
-				HashMap<P2PFile, ArrayList<SocketAddress>> listFiles = lfs.getListFiles();
-				
-				for (Entry<P2PFile, ArrayList<SocketAddress>> mapentry : listFiles.entrySet()) {
-					P2PFile file = mapentry.getKey();
-					String nameFile = file.getNameFile();
-					if (nameFile.indexOf(motif) != -1) {
-						currentSearch.add(file);
-					}
-				}
-				
-				System.out.println(currentSearch.size());
-				P2PFile [] currentSearchArray = new P2PFile[currentSearch.size()];
-				currentSearch.toArray(currentSearchArray);
-				
-				sockOs.writeObject(currentSearchArray);
-				sockOs.flush();
-				break;
-				
-			case "get":
-				
-				break;
-				
-			default:
-				break;
-			}
+
+            Request requete;
+
+            do {
+                requete = (Request) sockIn.readObject();
+                ArrayList<P2PFile> currentSearch = new ArrayList<P2PFile>();
+                String commande = requete.getCommande();
+
+                switch (commande) {
+                    case "list":
+                        sockOs.writeObject(currentSearch);
+                        sockOs.flush();
+                        break;
+
+                    case "help":
+                        sockOs.writeUTF(" search <pattern> \n get <num> \n list \n local list \n quit");
+                        sockOs.flush();
+                        break;
+
+                    case "search":
+                        String motif = requete.getArg();
+                        System.out.println(motif);
+                        HashMap<P2PFile, ArrayList<SocketAddress>> listFiles = lfs.getListFiles();
+
+                        for (Entry<P2PFile, ArrayList<SocketAddress>> mapentry : listFiles.entrySet()) {
+                            P2PFile file = mapentry.getKey();
+                            String nameFile = file.getNameFile();
+                            if (nameFile.indexOf(motif) != -1) {
+                                currentSearch.add(file);
+                            }
+                        }
+
+                        System.out.println(currentSearch.size());
+                        P2PFile[] currentSearchArray = new P2PFile[currentSearch.size()];
+                        currentSearch.toArray(currentSearchArray);
+
+                        sockOs.writeObject(currentSearchArray);
+                        sockOs.flush();
+                        break;
+
+                    case "get":
+
+                        break;
+
+                    default:
+                        break;
+                }
+
+            }while (requete != null);
         } 
         catch (EOFException e){
             System.out.println("Fin de la connection avec le  client : " +
